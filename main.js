dacda130ba5ac65fb0d963dc18bcd06927b6d8bd
shm_usuf <shmusuf@gmail.com>
2023-04-07 20:00:23 +0600

    short circuit nullish operator

--- main.js
+++ main.js
@@ -46,3 +46,21 @@ const user = {
 
 const { name, skills, ...rest } = user;
 console.log(name, skills, rest); */
+
+// 05. Short Circuiting (&& and ||)
+// let money = 100;
+// money >= 50 && console.log("I am rich");
+
+// 06. The Nullish Coalescing Operator (??)
+/* let money = 0;
+money ?? (money = 10);
+console.log(money); */
+
+// 07. Enhanced Object Literals
+/* let burgerPrice = 250;
+const restaurant = {
+  name: "Puran Dhaka Burger House",
+  burgerPrice,
+};
+
+console.log(restaurant); */
