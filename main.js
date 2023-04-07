5def69efd64b9d4bf01b40c1d55f0aae84c21157
shm_usuf <shmusuf@gmail.com>
2023-04-07 12:10:42 +0600

    destructuring, spread and rest added

--- main.js
+++ main.js
@@ -14,9 +14,35 @@ const { fName, lName } = student;
 console.log(fName, lName); */
 
 //03. The Spread Operator (...)
-const arr = [1, 3, 4, 5, 6, 9];
+/* const arr = [1, 3, 4, 5, 6, 9];
 const newArr = [...arr];
 newArr.pop();
 
 console.log(arr);
 console.log(newArr);
+
+const human = {
+  name: "Jhon Doe",
+  lang: "English",
+};
+
+const animal = { ...human };
+animal.name = "Tiger";
+animal.lang = "null";
+console.log(human);
+console.log(animal); */
+
+//04. Rest Pattern and Parameters
+/* const friends = ["atik", "jony", "arif", "alia"];
+const [closeFriend, ...others] = friends;
+// console.log(closeFriend, others);
+
+const user = {
+  name: "John",
+  age: 25,
+  skills: "programmer",
+  income: 15000,
+};
+
+const { name, skills, ...rest } = user;
+console.log(name, skills, rest); */
