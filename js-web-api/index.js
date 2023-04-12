5a34f78c7e2b2762ab9c511f4f1d2bfa53c94f68
shm_usuf <shmusuf@gmail.com>
2023-04-12 15:16:35 +0600

    Math added

---
+++ js-web-api/index.js
@@ -0,0 +1,31 @@
+// Numeric JavaScript + Common WebAPIs
+
+// Math.abs()
+// console.log(Math.abs(-1)); //1
+// Math.ceil()
+// console.log(Math.ceil(4.001));//5
+// Math.floor()
+// console.log(Math.floor(3.9));//3
+// Math.round()
+// console.log(Math.round(3.9)); //4
+// console.log(Math.round(3.4)); //3
+// Math.max()
+// console.log(Math.max(...[5, 7, 3, 8, 4])); //8
+// Math.min()
+// console.log(Math.min(...[5, 7, 3, 8, 4])); //3
+// Math.pow()
+// console.log(Math.pow(2, 3)); //8
+// console.log(3 ** 3); //27
+// Math.sqrt()
+// console.log(Math.sqrt(25));
+// console.log(Math.floor(Math.sqrt(24)));
+// Math.exp()
+// Math.log()
+// Math.sin()
+// Math.cos()
+// Math.tan()
+// Math.random()
+// console.log(Math.floor(Math.random() * 10 + 1));
+// Math.trunc()
+// console.log(Math.trunc(2.7)); //2
+// console.log(Math.trunc(5.4)); //5
