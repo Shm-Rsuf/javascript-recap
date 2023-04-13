322cd9f7e6bf6f9d4ac2000b127aab6ef7b4ae18
shm_usuf <shmusuf@gmail.com>
2023-04-13 21:22:22 +0600

    video added

--- advance-dom/index.js
+++ advance-dom/index.js
@@ -1,1 +1,15 @@
-console.log("check");
+const videoEl = document.querySelector(".video");
+// console.log(videoEl);
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((output) => {
+      videoEl.src = output;
+      videoEl.play();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+} else {
+  console.log("getUserMedia is not supported");
+}
