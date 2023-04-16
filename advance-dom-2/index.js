96e576221e929147413e179355233c612de8f67e
shm_usuf <shmusuf@gmail.com>
2023-04-16 21:45:33 +0600

    smooth scrolling added

--- advance-dom-2/index.js
+++ advance-dom-2/index.js
@@ -1,7 +1,24 @@
+"use strict";
+
+/* ELEMENTS */
 const frame = document.querySelector(".frame");
 const prevBtn = document.querySelector(".previous-btn");
 const nextBtn = document.querySelector(".next-btn");
+const navLinks = document.querySelector(".nav-links");
 
+/* SMOOTH SCROLLING */
+navLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav-item")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
+/* SLIDERS */
 let currentSlide = 0;
 const lastSlide = 2;
 
