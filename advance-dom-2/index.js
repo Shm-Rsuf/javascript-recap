b3b621ed38f8a0f4a715eb75d83b4611caad78ef
shm_usuf <shmusuf@gmail.com>
2023-04-16 21:00:32 +0600

    slider btn active

--- advance-dom-2/index.js
+++ advance-dom-2/index.js
@@ -1,1 +1,29 @@
-console.log("advance dom 2");
+const frame = document.querySelector(".frame");
+const prevBtn = document.querySelector(".previous-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let currentSlide = 0;
+const lastSlide = 2;
+
+function transformFrame(slide) {
+  frame.style.transform = `translateX(-${100 * slide}vw)`;
+}
+
+prevBtn.addEventListener("click", () => {
+  if (currentSlide === 0) {
+    currentSlide = lastSlide;
+    transformFrame(lastSlide);
+  } else {
+    currentSlide--;
+    transformFrame(currentSlide);
+  }
+});
+nextBtn.addEventListener("click", () => {
+  if (currentSlide === lastSlide) {
+    currentSlide = 0;
+    transformFrame(currentSlide);
+  } else {
+    currentSlide++;
+    transformFrame(currentSlide);
+  }
+});
