9de274f55356bdaeb2d964dfe487441f74cda8b9
shm_usuf <shmusuf@gmail.com>
2023-04-17 13:34:24 +0600

    prototype added

--- js-oop/index.js
+++ js-oop/index.js
@@ -16,3 +16,20 @@ console.log(person1.name);
 console.log(person2.name);
 
 console.log(Person.prototype); */
+
+/* 04. Prototypes */
+/* function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const person1 = new Person("shm usuf", 26);
+
+// console.log(Person.prototype); //prototype is used by constructor, to view it's prototype
+// console.log(person1.__proto__); //__proto__ is used by instances, to view it's constructor's prototype
+
+// console.log(Person.__proto__);
+// console.log(person1.prototype); */
+
+// 05. Prototypal Inheritance and The Prototype Chain
+// 06. Prototypal Inheritance on Built-In Objects
