8d69cda2b4348aa848392a82700edf96793fa680
shm_usuf <shmusuf@gmail.com>
2023-04-17 13:21:04 +0600

    constructor added

---
+++ js-oop/index.js
@@ -0,0 +1,18 @@
+// 01. What is Object-Oriented Programming?
+// 02. OOP in JavaScript
+// 03. Constructor Functions and the new Operator
+
+/* CONSTRUCTOR */
+/* function Person(name, age) {
+  this.name = name;
+  this.age = age;
+} */
+
+/* INSTANCE */
+/* const person1 = new Person("shohan", 25);
+const person2 = new Person("usuf", 26);
+
+console.log(person1.name);
+console.log(person2.name);
+
+console.log(Person.prototype); */
