9d6488da7c3455500559ffd40d6108d0690a8630
shm_usuf <shmusuf@gmail.com>
2023-04-20 23:50:55 +0600

    ajax added

--- js-ajax/index.js
+++ js-ajax/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("test");
+const getProducts = async () => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    if (!res.ok) throw new Error("Response failed");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+getProducts();
