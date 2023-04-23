8106b41e3bcb9d1a6e17341c51e16895843424f1
shm_usuf <shmusuf@gmail.com>
2023-04-23 22:11:41 +0600

    json data added

--- json/index.js
+++ json/index.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw new Error("Data not fetched");
+  const data = await res.json();
+  console.log(data);
+};
+getData();
