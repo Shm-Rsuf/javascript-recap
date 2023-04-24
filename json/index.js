d8bdddf90f73eec5c0d275726f43a7b779b3696c
shm_usuf <shmusuf@gmail.com>
2023-04-24 19:06:58 +0600

    try catch added

--- json/index.js
+++ json/index.js
@@ -1,7 +1,11 @@
 const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  if (!res.ok) throw new Error("Data not fetched");
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!res.ok) throw new Error("Data not fetched");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 getData();
