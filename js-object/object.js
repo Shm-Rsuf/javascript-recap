a815e542826972089a8c407abeca8d4c44d3e751
shm_usuf <shmusuf@gmail.com>
2023-06-13 22:44:45 +0600

    js obj added

--- js-object/object.js
+++ js-object/object.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("Hello");
+/* const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+  display: function () {
+    return `${this.firstName} ${this.lastName} ${this.age}`;
+  },
+};
+
+console.log(person.display()); */
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+
+for (let properties in person) {
+  console.log(properties);
+}
