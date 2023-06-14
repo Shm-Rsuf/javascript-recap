3bb52101c19c428c22431f5a6eb1ac65f66710a4
shm_usuf <shmusuf@gmail.com>
2023-06-14 17:30:53 +0600

    commit

--- js-object/object.js
+++ js-object/object.js
@@ -20,3 +20,35 @@ console.log(person.display()); */
 for (let properties in person) {
   console.log(properties);
 } */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+/* const climbStairs = (n) => {
+  if (n === 1 || n === 0) return 1;
+  let first = 1;
+  let second = 2;
+  for (let i = 3; i <= n; i++) {
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+  return second;
+};
+
+console.log(climbStairs(4)); */
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+  let seuareRoot = num ** 0.5;
+  if (seuareRoot % 1 === 0) {
+    return true;
+  }
+  return false;
+};
+
+console.log(isPerfectSquare(16));
+console.log(isPerfectSquare(14));
