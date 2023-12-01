f1baef995e160bca4183840f4279cb8388ec5721
shm rsuf <shmusuf@gmail.com>
2023-12-01 09:29:04 +0600

    video 2 added

---
+++ js-tips_triks/tips2.js
@@ -0,0 +1,69 @@
+const arr = [1, 2, 3, 4, 5];
+
+/* swap form temp variable */
+// const temp = arr[0];
+// arr[0] = arr[4];
+// arr[4] = temp;
+// console.log(arr);
+
+// [arr[0], arr[4]]=[arr[4], arr[0]];
+// console.log(arr);
+
+/* copy text */
+// function copyToClipBoard(str){
+//   const element = document.createElement("textarea");
+//   element.value = str;
+//   document.body.appendChild(element);
+//   element.select();
+//   document.execCommand("copy");
+//   document.body.removeChild(element);
+// }
+
+// function handleClick(){
+//   let text = document.querySelector("#text");
+//   copyToClipBoard(text.innerText);
+// }
+
+/* destructuring aliases */
+// const language = {
+//   name: "javascript",
+//   founded:1996,
+//   founder:"Brendan Eich"
+// };
+
+// const {name: languageName, founder: createrName}= language;
+
+// console.log({languageName, createrName});
+
+/* get value as data type */
+// const element = document.querySelector("#number").valueAsNumber;
+// console.log(element);
+
+/* remove duplicate value from an array */
+// const duplicateArr = [1, 2, 2, 1, 3, 4, 3, 5, 3, 4];
+
+// console.log([...new Set(duplicateArr)]);
+
+/* compare two array by value */
+const hasSameElement=(a, b)=>{
+  return a.length === b.length && a.every((val, i)=> val === b[i]);
+}
+
+console.log(hasSameElement([1, 3, 4],[1, 3, 4]));
+
+/* suffling array */
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.sort(()=>Math.random()-.5));
+/* comma operator in javascript */
+let x = 1;
+x = (x++, x+3);
+console.log(x);
+
+const a = Array.from({ length: 10 }, () =>
+  Array.from({ length: 10 }, Math.random),
+); // A 10×10 array of random numbers
+
+for (let i = 0, j = 9; i <= 9; i++, j--) {
+  console.log(`a[${i}][${j}] = ${a[i][j]}`);
+}
