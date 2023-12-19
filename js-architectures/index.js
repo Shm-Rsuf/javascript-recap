a8285595b4e4662c37c8cc9eaaef079509abf60f
shm rsuf <shmusuf@gmail.com>
2023-12-19 23:26:20 +0600

    assignment destructuring added

--- js-architectures/index.js
+++ js-architectures/index.js
@@ -53,18 +53,70 @@ console.log(Calculations.add(4, 6));
  */
 
 /* template literal in javascript */
-let name = "usuf ali",
-  age = 27,
-  job = "vivasoft";
+// let name = "usuf ali",
+//   age = 27,
+//   job = "vivasoft";
 
-console.log(`My name is ${name}. I\'m ${age} years old & I work at ${job}`);
+// console.log(`My name is ${name}. I\'m ${age} years old & I work at ${job}`);
 
-const myInfo = {
-  name: "usuf ali momin",
-  age: 27,
-  job: "ollyo bangladesh",
-};
+// const myInfo = {
+//   name: "usuf ali momin",
+//   age: 27,
+//   job: "ollyo bangladesh",
+// };
 
-console.log(
-  `My name is ${myInfo.name}. I\'m ${myInfo.age} years old & I work at ${myInfo.job}`
-);
+// console.log(
+//   `My name is ${myInfo.name}. I\'m ${myInfo.age} years old & I work at ${myInfo.job}`
+// );
+
+/* Assignment Destructuring */
+// const companyInfo = {
+//   name: "vivasoft",
+//   service: "software company",
+//   loc: "Dhaka",
+// };
+
+// const { name: companyName, loc: companyLocation } = companyInfo;
+// console.log(companyName);
+// console.log(companyLocation);
+
+// function test(a) {
+//   console.log(`${a} is an ${a % 2 ? "odd" : "even"} number`);
+// }
+
+// test(1);
+// test(10);
+
+// var a = 1;
+// function varTest() {
+//   a = 3;
+//   var a = 2;
+//   console.log({ a });
+//   var x = 4;
+// }
+// varTest();
+// console.log(a);
+// console.log(x);
+
+// console.log(c);
+// const c = 4;
+// function constTest() {
+//   console.log(c);
+//   const c = 5;
+//   console.log(c);
+// }
+// constTest();
+// c = 6;
+// console.log(c);
+
+// console.log(b);
+// let b = 4;
+// function letTest() {
+//   console.log(b);
+//   let b = 5;
+//   console.log(b);
+// }
+// letTest();
+
+console.log(`${d ? d : -1}`);
+var d = 7;
